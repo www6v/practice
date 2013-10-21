d77a5b126c3fee5066bc71bc7b370f27aa30b4e1
u011288273 <carlyk@163.com>
2013-10-21 14:01:20 +0800

    Binding Annotations
    Binding Annotations

--- src/main/java/com/zoo/guice/userguide/chap2/HelloServiceModule2.java
+++ src/main/java/com/zoo/guice/userguide/chap2/HelloServiceModule2.java
@@ -7,8 +7,8 @@ public class HelloServiceModule2 extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		bind(Animal.class).to(Cat.class);
 		bind(Animal.class).annotatedWith(Names.named("Snoopy")).to(Dog.class);
+		bind(Animal.class).to(Cat.class);
 		bind(HelloService.class).to(HelloServiceImpl2.class);
 	}
 
