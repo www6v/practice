c81a0bde13c78a5a4cf03b7ad8470407e5c5baaa
u011288273 <carlyk@163.com>
2013-10-22 12:47:54 +0800

    Instance Bindings
    Instance Bindings

---
+++ src/main/java/com/zoo/guice/userguide/chap3/ConfigureModule.java
@@ -0,0 +1,18 @@
+package com.zoo.guice.userguide.chap3;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+
+public class ConfigureModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		bind(String.class)
+	        .annotatedWith(Names.named("JDBC URL"))
+	        .toInstance("jdbc:mysql://localhost/pizza");
+		bind(Integer.class)
+	        .annotatedWith(Names.named("login timeout seconds"))
+	        .toInstance(10);		
+	}
+
+}
